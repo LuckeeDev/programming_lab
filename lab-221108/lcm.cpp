2f215ca0bc37e548c4db16a40a25d096beb79980
Luca Zoppetti <luca@luckee.dev>
2022-11-08 12:08:54 +0100

    implement lcm and tests

---
+++ lab-221108/lcm.cpp
@@ -0,0 +1,22 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <cmath>
+
+#include "doctest.h"
+
+int gcd(int a, int b) {
+  if (b == 0) {
+    return a;
+  } else {
+    return gcd(b, a % b);
+  }
+}
+
+int lcm(int a, int b) { return abs(a * b) / gcd(a, b); }
+
+TEST_CASE("Testin least common multiple") {
+  CHECK(lcm(12, 16) == 48);
+  CHECK(lcm(26, 15) == 390);
+  CHECK(lcm(6, 6) == 6);
+  CHECK(lcm(-12, 6) == 12);
+  CHECK(lcm(0, 123123) == 0);
+}
